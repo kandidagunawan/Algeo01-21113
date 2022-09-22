875be9669dc037a41d4f1c4678da39d9a5057460
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2022-09-22 16:38:34 +0700

    add simpleOperation package + initialize simpleOperation function
    transpose & isIdentity done

--- Algeo01_21113/src/Main.java
+++ Algeo01_21113/src/Main.java
@@ -1,7 +1,35 @@
+
 import java.util.Scanner;
+import simpleOperation.*;
+
+import simpleOperation.simpleOperation;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world Dida!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Masukkan jumlah baris matrix: ");
+		int rows = scanner.nextInt();
+		System.out.println("Masukkan jumlah kolom matrix: ");
+		int cols = scanner.nextInt();
+		double [][] matrix = new double [rows][cols];
+		
+		System.out.println("Masukkan elemen matrix: ");
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				matrix[i][j] = scanner.nextInt();
+			}
+		}
+		simpleOperation simple = new simpleOperation();
+//		result = simple.transpose(matrix);
+//		simple.printMatrix(result);
+//		System.out.println(simple.isIdentity(matrix));
+//		
+		
+
+
+	
+		
 	}
+	
 }
---
+++ Algeo01_21113/src/simpleOperation/simpleOperation.java
@@ -0,0 +1,124 @@
+package simpleOperation;
+
+public class simpleOperation {
+	
+	public void printMatrix(double [][]matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	// CEK MATRIX
+	public boolean isIdentity(double [][] matrix) {
+		if(isSquare(matrix)) {
+			int n = matrix.length;
+			for(int i = 0; i < matrix.length; i++) {
+				for(int j = 0; j < matrix.length; j++) {
+					if(i==j) {
+						if(matrix[i][j] != 1) {
+							return false;
+						}
+					}
+					else {
+						if(matrix[i][j] != 0) {
+							return false;
+						}
+					}
+				}
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	public boolean isSquare(double [][] matrix) {
+		return true; // ini gw isi true supaya ga error aj di eclipsenya, nanti realisasi sendiri y
+	}
+	
+	
+	// OPERASI SEDERHANA PADA MATRIX
+	public double[][]plusMinMatrix(double[][]matrix1, double[][]matrix2){
+		double[][]result = null;
+		return result;
+	}
+	public double[][] perkalianDuaMatrix(double[][]matrix1, double[][]matrix2){
+		double[][]result = null;
+		return result;
+	}
+	public double [][] perkalianMatrixConst(double[][]matrix, float con){
+		double[][] result = null;
+		return result;
+	}
+	
+	public double[][]transpose(double[][]matrix){
+		int rows = matrix.length;
+		int cols = matrix[0].length;	
+		double [][] result = new double [cols][rows];
+
+		for (int i = 0; i < cols; i++) {
+			for(int j = 0; j < rows; j++) {
+				result[i][j] = matrix[j][i];
+			}
+		}
+		return result;
+	}
+	
+	public double[][]matrixKofaktor(double[][]matrix){
+		double [][] result = null;
+		return result;
+	}
+	
+//	ELIMINASI GAUSS & GAUSS JORDAN
+	public double[][]gauss(double[][]matrix){
+		double[][] result = null;
+		return result;
+	}
+	public double[][]gaussJordan(double[][]matrix){
+		double[][]result = null;
+		return result;
+	}
+	
+	
+//	DETERMINAN MATRIX
+	
+	// Determinan matrix dengan reduksi baris OBE
+	public double determinanOBE(double[][]matrix) {
+		double result = 0;
+		return result;
+	}
+	
+	//Determinan matrix dengan ekspansi kofaktor
+	public double determinanKofaktor(double[][]matrix) {
+		double result = 0;
+		return result;
+	}
+	
+// INVERS MATRIX
+	
+	//	Matrix invers dengan gauss jordan
+	public double[][]inversGaussJordan(double[][]matrix){
+		double [][]result = null;
+		return result;
+	}
+	
+	//	Matrix invers dengan adjoin
+	public double[][]inversWithAdjoin(double[][]matrix){
+		double[][]result = null;
+		return result;
+	}
+	
+
+
+	
+	
+	
+	
+	
+}
