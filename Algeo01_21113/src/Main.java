3d94ded4277f478d655132f1161924f985c2353f
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2022-09-29 22:07:50 +0700

    buat menu + udah bisa input dari keyboard
    beberapa menu, fungsinya masih belum dimasukin

--- Algeo01_21113/src/Main.java
+++ Algeo01_21113/src/Main.java
@@ -6,34 +6,148 @@ import simpleOperation.simpleOperation;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world Dida!");
+		simpleOperation simple = new simpleOperation();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Masukkan jumlah baris matrix: ");
-		int rows = scanner.nextInt();
-		System.out.println("Masukkan jumlah kolom matrix: ");
-		int cols = scanner.nextInt();
-		double [][] m = new double [rows][cols];
-		
-		System.out.println("Masukkan elemen matrix: ");
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < cols; j++) {
-				m[i][j] = scanner.nextInt();
+		boolean running = true;
+		while(running) {
+			System.out.println("MENU");
+			System.out.println("1. Sistem Persamaan Linier");
+			System.out.println("2. Determinan");
+			System.out.println("3. Matriks balikan");
+			System.out.println("4. Interpolasi Polinom");
+			System.out.println("5. Interpolasi Bicubic");
+			System.out.println("6. Regresi linier berganda");
+			System.out.println("7. Keluar");
+			System.out.println("Silahkan pilih menu: ");
+			int choice = scanner.nextInt();
+			if(choice == 1) {
+				System.out.println("Masukkan banyak persamaan yang akan diinputkan (m): ");
+				int m = scanner.nextInt();
+				System.out.println("Masukkan banyak variabel yang dipakai(n): ");
+				int n = scanner.nextInt();
+				System.out.println("Silahkan masukkan persamaan SPL : ");
+				double matrix[][] = new double[m][n];
+				for(int i = 0; i < m; i++) {
+					for(int j = 0; j < n; j++) {
+						matrix[i][j] = scanner.nextDouble();
+					}
+				}
+				System.out.println("Silahkan pilih metode penyelesaian SPL: ");
+				System.out.println("1. Metode eliminasi Gauss");
+				System.out.println("2. Metode eliminasi Gauss-Jordan");
+				System.out.println("3. Metode matriks balikan");
+				System.out.println("4. Kaidah cramer");
+				int metodeSPL = scanner.nextInt();
+				if(metodeSPL == 1) {
+					
+				}
+				else if(metodeSPL == 2) {
+					
+				}
+				else if(metodeSPL == 3) {
+					
+				}
+				else if(metodeSPL == 4) {
+					
+				}
+				
+			}
+			else if(choice == 2) {
+				System.out.println("Silahkan masukkan orde matrix: ");
+				int n = scanner.nextInt();
+				System.out.println("Silahkan masukkan matrix anda: ");
+				double matrix[][] = new double[n][n];
+				for(int i = 0; i < n; i++) {
+					for(int j = 0; j < n; j++) {
+						matrix[i][j] = scanner.nextDouble();
+					}
+				}
+				System.out.println("Pilih cara menentukan determinan: ");
+				System.out.println("1. Metode Gauss Jordan");
+				System.out.println("2. Metode Kofaktor");
+				int metodeMatrix = scanner.nextInt();
+				if(metodeMatrix == 1) {
+					System.out.println("Nilai determinan matrix : " + simple.determinanOBE(matrix));
+				}
+				else if(metodeMatrix == 2) {
+					System.out.println("Nilai determinan matrix : " + simple.determinanKofaktor(matrix));
+				}
+				else {
+					System.out.println("Anda memilih pilihan yang salah");
+				}
+
+				
+			}
+			else if(choice == 3) {
+				System.out.println("Silahkan masukkan orde matrix: ");
+				int n = scanner.nextInt();
+				System.out.println("Silahkan masukkan matrix anda: ");
+				double matrix[][] = new double[n][n];
+				for(int i = 0; i < n; i++) {
+					for(int j = 0; j < n; j++) {
+						matrix[i][j] = scanner.nextDouble();
+					}
+				}
+				System.out.println("Pilih cara menentukan invers: ");
+				System.out.println("1. Invers dengan Gauss Jordan");
+				System.out.println("2. Invers dengan matrix adjoin");
+				int metodeMatrix = scanner.nextInt();
+				if(metodeMatrix == 1) {
+					simple.printMatrix(simple.inversGaussJordan(matrix));
+				}
+				else if(metodeMatrix == 2) {
+					simple.printMatrix(simple.inversWithAdjoin(matrix));
+				}
+				else {
+					System.out.println("Anda memilih pilihan yang salah");
+				}
+			}
+			else if(choice == 4) {
+				System.out.println("Silahkan masukkan banyak titik yang ingin diinterpolasi : ");
+				int n = scanner.nextInt();
+				double matrix[][] = new double[n][2];
+				for(int i = 0; i < n; i++) {
+					for(int j = 0; j < 2; j++) {
+						matrix[i][j] = scanner.nextDouble();
+					}
+				}
+				// INI MASUKIN AJ FUNGSI INTERPOLASINYA BUAT NYARI LALLAA
+			}
+			else if(choice == 5) {
+				double matrix[][] = new double[4][4];
+				System.out.println("Silahkan masukkan matrix 4x4: ");
+				for(int i = 0; i < 4; i++) {
+					for(int j = 0; j < 4; j++) {
+						matrix[i][j] = scanner.nextDouble();
+					}
+				}
+				System.out.println("Silahkkan masukkan titik yang ingin dicari nilainya: ");
+				double x = scanner.nextDouble();
+				double y = scanner.nextDouble();
+				// MASUKIN RUMUS BICUBIC
+				
+				
+			}
+			else if(choice == 6) {
+				System.out.println("Silahkan masukkan jumlah peubah x (n): ");
+				int n = scanner.nextInt();
+				System.out.println("Silahkan masukkan jumlah sample (m)");
+				int m = scanner.nextInt();
+				// to be continued ini si regresi
+				
+				
+			}
+			else if(choice == 7) {
+				running = false;
+				System.out.println("Sampai jumpa lagi");
+			}
+			else {
+				System.out.println("Menu yang anda pilih tidak ada");
 			}
 		}
-		simpleOperation simple = new simpleOperation();
-//		double [][] matrixGauss = new double[rows][cols];
-//		double [][] matrixJordan = new double[rows][cols];
-//		double [][] matrixInverse = new double[rows][cols];
-//		matrixGauss = simple.gauss(m);
-//		System.out.println("Matrix hasil gauss: \n");
-//		simple.printMatrix(matrixGauss);
-//		System.out.println("Matrix hasil gauss jordan: \n");
-//		simple.printMatrix(matrixJordan);
-//		matrixInverse = simple.inversGaussJordan(m);
-		double determinan = simple.determinanOBE(m);
-		System.out.println(determinan);
+
 		
-//		
+
 
 	}
 	
