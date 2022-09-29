df35dd077276cc5c904f147d7467942b7fef0373
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2022-09-30 01:11:17 +0700

    buat matriks persamaan buat mencari koefisiennya

--- Algeo01_21113/src/Main.java
+++ Algeo01_21113/src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
+import complexOperation.*;
 import simpleOperation.*;
 
-import simpleOperation.simpleOperation;
+//import simpleOperation.simpleOperation;
 
 public class Main {
 	public static void main(String[] args) {
 //		System.out.println("Hello world Dida!");
-//		Scanner scanner = new Scanner(System.in);
+	
+		Scanner scanner = new Scanner(System.in);
 //		System.out.println("Masukkan jumlah baris matrix: ");
 //		int rows = scanner.nextInt();
 //		System.out.println("Masukkan jumlah kolom matrix: ");
@@ -19,7 +21,8 @@ public class Main {
 //				m[i][j] = scanner.nextInt();
 //			}
 //		}
-//		simpleOperation simple = new simpleOperation();
+		simpleOperation simple = new simpleOperation();
+		regresiLinier regresi = new regresiLinier();
 //		double [][] matrixGauss = new double[rows][cols];
 //		double [][] matrixJordan = new double[rows][cols];
 //		double [][] matrixInverse = new double[rows][cols];
@@ -31,76 +34,84 @@ public class Main {
 //		matrixInverse = simple.inversGaussJordan(m);
 //		double determinan = simple.determinanOBE(m);
 //		System.out.println(determinan);
-		double[][]X = new double[16][16];
-		int[][]pasanganxy = new int[16][2];
-		int x = -1;
-		int y = -1;
-		int countx = 0;
-		int county = 0;
-		for(int i = 0; i < 16; i++) {
-			for(int j = 0; j < 2; j++) {
-				if(j == 0) {
-					pasanganxy[i][j] = x;
-					x++;
-					if(x > 2) {
-						x = -1;
-					}	
-				}
-				else {
-					pasanganxy[i][j] = y;
-					county++;
-					if(county == 4) {
-						y++;
-						county = 0;
-					}
-				}
-			}
-
-		}
-		
-		
-		int [][]pasanganij = new int[16][2];
-		int x1 = 0;
-		int y1 = 0;
-		int county1 = 0;
-		for(int i = 0; i < 16; i++) {
-			for(int j = 0; j < 2; j++) {
-				if(j == 0) {
-					pasanganij[i][j] = x1;
-					x1++;
-					if(x1 > 3) {
-						x1 = 0;
-					}
-				}
-				else {
-					pasanganij[i][j] = y1;
-					county1++;
-					if(county1 == 4) {
-						y1++;
-						county1 = 0;
-					}
-				}
-			}
-		}
-
-		for(int i = 0; i < 16; i++) {
-			for(int j = 0; j < 16; j++) {
-				X[i][j] = Math.pow(pasanganxy[i][0], pasanganij[j][0]) * Math.pow(pasanganxy[i][1], pasanganij[j][1]);
-				
-			}
-		}
-//		for(int k = 0; k < 16; k++) {
-//			for(int l = 0; l < 16; l++) {
-//				X[k][l] = Math.pow(k, l);
+//		double[][]X = new double[16][16];
+//		int[][]pasanganxy = new int[16][2];
+//		int x = -1;
+//		int y = -1;
+//		int countx = 0;
+//		int county = 0;
+//		for(int i = 0; i < 16; i++) {
+//			for(int j = 0; j < 2; j++) {
+//				if(j == 0) {
+//					pasanganxy[i][j] = x;
+//					x++;
+//					if(x > 2) {
+//						x = -1;
+//					}	
+//				}
+//				else {
+//					pasanganxy[i][j] = y;
+//					county++;
+//					if(county == 4) {
+//						y++;
+//						county = 0;
+//					}
+//				}
+//			}
+//
+//		}
+//		
+//		
+//		int [][]pasanganij = new int[16][2];
+//		int x1 = 0;
+//		int y1 = 0;
+//		int county1 = 0;
+//		for(int i = 0; i < 16; i++) {
+//			for(int j = 0; j < 2; j++) {
+//				if(j == 0) {
+//					pasanganij[i][j] = x1;
+//					x1++;
+//					if(x1 > 3) {
+//						x1 = 0;
+//					}
+//				}
+//				else {
+//					pasanganij[i][j] = y1;
+//					county1++;
+//					if(county1 == 4) {
+//						y1++;
+//						county1 = 0;
+//					}
+//				}
 //			}
 //		}
+//
+//		for(int i = 0; i < 16; i++) {
+//			for(int j = 0; j < 16; j++) {
+//				X[i][j] = Math.pow(pasanganxy[i][0], pasanganij[j][0]) * Math.pow(pasanganxy[i][1], pasanganij[j][1]);
+//				
+//			}
+//		}
+////		for(int k = 0; k < 16; k++) {
+////			for(int l = 0; l < 16; l++) {
+////				X[k][l] = Math.pow(k, l);
+////			}
+////		}
+//		
+//		for(int i = 0; i < 16; i++) {
+//			for(int j = 0; j < 16; j++) {
+//				System.out.print(X[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
 		
-		for(int i = 0; i < 16; i++) {
-			for(int j = 0; j < 16; j++) {
-				System.out.print(X[i][j] + " ");
+		double[][] test = new double[20][4];
+		for(int i = 0; i < 20; i++) {
+			for(int j = 0; j < 4; j++) {
+				test[i][j] = scanner.nextDouble();
 			}
-			System.out.println();
 		}
+		simple.printMatrix(regresi.matrixPersamaan(test));
 //		
 
 	}
--- Algeo01_21113/src/complexOperation/regresiLinier.java
+++ Algeo01_21113/src/complexOperation/regresiLinier.java
@@ -1,5 +1,39 @@
 package complexOperation;
 
 public class regresiLinier {
-
+	public double[][] matrixPersamaan(double matrix[][]){
+		int row = matrix.length;
+		int col = matrix[0].length;
+		int rowPersamaan = matrix[0].length;
+		int colPersamaan = matrix[0].length + 1;
+		double[][]persamaan = new double[rowPersamaan][colPersamaan];
+		int sampel = matrix.length;
+		
+		for(int i = 0; i < rowPersamaan; i++) {
+			for(int j = 0; j < colPersamaan; j++) {
+				persamaan[i][j] = 0;
+			}
+		}
+		persamaan[0][0] = sampel;
+		for(int j = 1; j < colPersamaan; j++) {
+			for(int k = 0; k < row; k++) {
+				persamaan[0][j] += matrix[k][j-1];
+			}
+		}
+		for(int i = 1; i < rowPersamaan; i++) {
+			for(int j = 0; j < row; j++) {
+				persamaan[i][0] += matrix[j][i-1];
+			}
+		}
+		for(int i = 1; i < rowPersamaan; i++) {
+			for(int j = 1; j < colPersamaan; j++) {
+				for(int k = 0; k < row; k++) {
+					persamaan[i][j] += (matrix[k][i-1] * matrix[k][j-1]);
+				}
+			}
+		}
+		
+		return persamaan;
+	}
+	
 }
