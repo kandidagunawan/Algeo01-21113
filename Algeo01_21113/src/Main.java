8a18706a00416b871a0f6e8847e7af0c8a794dfe
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2022-10-01 18:53:47 +0700

    ngetes + solve bug di fungsi inputFile buat determinan & inverse

--- Algeo01_21113/src/Main.java
+++ Algeo01_21113/src/Main.java
@@ -13,15 +13,68 @@ import simpleOperation.simpleOperation;
 
 public class Main {
 	public static void main(String[] args) {
-		simpleOperation simple = new simpleOperation();
-		outputFile out = new outputFile();
 		Scanner scanner = new Scanner(System.in);
-		double[][] d = new double[3][3];
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				d[i][j] = scanner.nextDouble();
+		simpleOperation simple = new simpleOperation();
+		
+		double matrix[][] = null;
+		String directory;
+		System.out.println("Silahkan masukkan path dari file yang ingin diinputkan: ");
+		//  C:/Users/kandi/Downloads/mat.txt
+		directory = scanner.nextLine();
+		File temp = new File(directory);
+		Scanner file;
+		Scanner matrixIn;
+		int n = 0, m = 0;
+		try {
+			file = new Scanner(temp);
+			Scanner split = new Scanner(file.nextLine());
+			n = 0;
+			m = 1;
+			while(split.hasNextDouble()) {
+				n++;
+				//System.out.println(n);
+				split.nextDouble();
 			}
+			while(file.hasNextLine()) {
+				m++;
+				file.nextLine();
+			}
+			//System.out.println(m);
+			matrixIn = new Scanner(temp);
+			Scanner splitMatrix = new Scanner(matrixIn.nextLine());			
+			matrix = new double[m][n];
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < n; j++) {
+					matrix[i][j] = splitMatrix.nextDouble();
+				}
+				if(matrixIn.hasNextLine()) {
+					splitMatrix = new Scanner(matrixIn.nextLine());
+
+				}
+						
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+		
+//		outputFile out = new outputFile();
+		
+		
+//		double[][] d = new double[3][3];
+//		for(int i = 0; i < 3; i++) {
+//			for(int j = 0; j < 3; j++) {
+//				d[i][j] = scanner.nextDouble();
+//			}
+//		}
 //		System.out.println();
 //		double[][]kofaktor = new double[3][3];
 //		double[][]adjoin = new double[3][3];
@@ -33,10 +86,10 @@ public class Main {
 //		System.out.println(determinan);
 //		
 //		simple.printMatrix(simple.inversWithAdjoin(d));
-		out.invFile(d);
+//		out.invFile(d);
+
+			
 		
-		
-		
-	}
+	
 
 }
